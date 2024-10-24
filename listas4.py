7e3526e5ce97ddac15ae443cbf1a73e74fe4687a
Mario Rey <mario.rb.88@gmail.com>
2024-10-24 23:41:33 +0200

    listas

---
+++ listas4.py
@@ -0,0 +1,23 @@
+my_list = [8, 10, 6, 2, 4]
+my_list.sort()
+print(my_list)
+
+
+
+my_list = []
+swapped = True
+num = int(input("¿Cuántos elementos deseas ordenar?: "))
+
+for i in range(num):
+    val = float(input("Ingresa un elemento de la lista: "))
+    my_list.append(val)
+
+while swapped:
+    swapped = False
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            swapped = True
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print("\nOrdenada:")
+print(my_list)
